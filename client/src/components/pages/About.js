9405357d9033bb832bf3a34108a1f1ddce0a5e40
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-23 18:08:47 +0530

    Cleanup, NavBar & Router Setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p className='my-1'>Full Stack React app to manage contacts</p>
+			<p className='bg-dark p'>
+				<strong>Version: </strong> 1.0.0
+			</p>
+		</div>
+	);
+};
+
+export default Home;

17c7ae7f17b38a76cb0644d333cbba1e398bf1c5
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-27 17:50:08 +0530

    Logout & Navbar

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -75,7 +75,9 @@ const AuthState = (props) => {
 	};
 
 	//Logout
-	const logout = () => {};
+	const logout = () => {
+		dispatch({ type: LOGOUT });
+	};
 
 	//Clear Errors
 	const clearErrors = () => {

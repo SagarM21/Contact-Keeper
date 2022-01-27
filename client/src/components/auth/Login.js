5a19bc07e1e18ecc8b5c5f7d969d9f0feb32fc5d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-01-27 16:34:15 +0530

    User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
 const Login = () => {
+	const authContext = useContext(AuthContext);
+	const alertContext = useContext(AlertContext);
+	const { setAlert } = alertContext;
+	const { login, error, clearErrors, isAuthenticated } = authContext;
+
+	useEffect(() => {
+		//props.history.push does not work in react router dom updated version so use Navigate
+
+		if (error === "Invalid credentials") {
+			setAlert(error, "danger");
+			clearErrors();
+		}
+		// eslint-disable-next-line
+	}, [error, isAuthenticated]);
+
 	const [user, setUser] = useState({
 		email: "",
 		password: "",
@@ -13,8 +31,14 @@ const Login = () => {
 
 	const onSubmit = (e) => {
 		e.preventDefault();
-		//console.log("login sUBMIT");
+		if (email === "" || password === "") {
+			setAlert("Please fill in all fields", "danger");
+		} else {
+			login({ email, password });
+		}
 	};
+
+	if (isAuthenticated) return <Navigate to='/' />;
 
 	return (
 		<div className='form-container'>
@@ -24,7 +48,13 @@ const Login = () => {
 			<form onSubmit={onSubmit}>
 				<div className='form-group'>
 					<label htmlFor='email'>Email Address</label>
-					<input type='email' name='email' value={email} onChange={onChange} />
+					<input
+						type='email'
+						name='email'
+						value={email}
+						onChange={onChange}
+						required
+					/>
 				</div>
 				<div className='form-group'>
 					<label htmlFor='password'>Password</label>
@@ -33,6 +63,7 @@ const Login = () => {
 						name='password'
 						value={password}
 						onChange={onChange}
+						required
 					/>
 				</div>
 
